1949ebcb5013923fdf07b4fbfb22e0f6f172e3ae
deanoemcke <dean@thetransitapp.com>
2016-12-27 15:18:08 -0500

    Update grunt file

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,22 +3,28 @@ module.exports = function(grunt) {
     require('time-grunt')(grunt);
 
     grunt.initConfig({
-        pkg: grunt.file.readJSON('package.json'),
-        manifest: grunt.file.readJSON('src/manifest.json'),
-        crx: {
-            dev: {
-                src: [
-                    "src/**/*",
-                    "!**/html2canvas.js",
-                    "!**/Thumbs.db"
-                ],
-                dest: "build/crx/<%= pkg.name %>-<%= manifest.version %>-dev.crx",
-                zipDest: "build/zip/<%= pkg.name %>-<%= manifest.version %>-dev.zip",
-                options: {
-                    "privateKey": "key.pem"
-                }
-            }
+      pkg: grunt.file.readJSON('package.json'),
+      manifest: grunt.file.readJSON('src/manifest.json'),
+      crx: {
+        myPublicExtension: {
+          src: [
+              "src/**/*",
+              "!**/Thumbs.db"
+          ],
+          dest: "build/zip/<%= pkg.name %>-<%= manifest.version %>-dev.zip",
+        },
+
+        mySignedExtension: {
+          src: [
+              "src/**/*",
+              "!**/Thumbs.db"
+          ],
+          dest: "build/crx/<%= pkg.name %>-<%= manifest.version %>-dev.crx",
+          options: {
+            privateKey: "key.pem"
+          }
         }
+      }
     });
 
     grunt.loadNpmTasks('grunt-crx');
