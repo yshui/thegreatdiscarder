241004a112aba6afcc22c9b79913fd8cf335fa69
Yuxuan Shui <yshuiv7@gmail.com>
2017-01-26 14:12:59 -0500

    remove Google analytics

--- src/js/eventPage.js
+++ src/js/eventPage.js
@@ -31,16 +31,6 @@ var chargingMode = false;
 chrome.runtime.onStartup.addListener(function () {
   if (debug) { console.log('Extension started.'); }
 
-  // Standard Google Universal Analytics code
-  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
-  ga('create', 'UA-89460956-1', 'auto');
-  ga('set', 'checkProtocolTask', function(){});
-  ga('require', 'displayfeatures');
-  ga('send', 'pageview', '/eventPage.html');
-
   chrome.alarms.clearAll(function () {
     localStorage.setItem(TEMPORARY_WHITELIST, []);
     tabStates.clearTabStates(function () {
@@ -163,8 +153,7 @@ function isSpecialTab(tab) {
   if (url.indexOf('chrome-extension:') === 0 ||
       url.indexOf('chrome:') === 0 ||
       url.indexOf('chrome-devtools:') === 0 ||
-      url.indexOf('file:') === 0 ||
-      url.indexOf('chrome.google.com/webstore') >= 0) {
+      url.indexOf('file:') === 0) {
     return true;
   }
   return false;
